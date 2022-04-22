08ec71e5733eefd02f8fde6e44eceb4c125aa385
Andriy Tymchenko <silpol@gmail.com>
2022-04-22 13:02:05 +0300

    02 DOM work - submitted

---
+++ javascript/02_dom_work/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+
+const app = express();
+
+let port = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+
+app.listen(port);
+
+console.log("Running on port "+port);
+
