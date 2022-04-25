4712e9c86284eb672f00d7432770989509fb9d34
Andriy Tymchenko <silpol@gmail.com>
2022-04-25 13:40:43 +0300

    submit project 04 simple spa - CRUD present; i++ fixed

--- javascript/04_simple_spa/server.js
+++ javascript/04_simple_spa/server.js
@@ -19,7 +19,7 @@ app.post("/api/contact", function(req, res){
         email:req.body.email,
         phone:req.body.phone
     }
-    id++
+    id++;
     database.push(contact);
     return res.status(201).json(contact);
 })
@@ -29,6 +29,24 @@ app.delete("/api/contact/:id", function(req,res) {
     let tempDatabase = database.filter(contact => contact.id !== tempId)
     database = tempDatabase;
     return res.status(200).json({message:"success!"});
+})
+
+app.put("/api/contact/:id", function(req, res) {
+    let tempId = parseInt(req.params.id, 10);
+    for(let i=0;i<database.length;i++) {
+        if(tempId === database[i].id) {
+            let contact = {
+                id:tempId,
+                firstname:req.body.firstname,
+                lastname:req.body.lastname,
+                email:req.body.email,
+                phone:req.body.phone
+            }
+            database.splice(i,1,contact);
+            return res.status(200).json({message:"success!"});
+        }
+    }
+    return res.status(404).json({message:"not found!"});
 })
 
 app.listen(3000);
