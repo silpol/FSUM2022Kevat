d6e757b5b843ca6241916dcffdbda0c7856746bd
Erno Hentonen <erno.hentonen@opiframe.com>
2022-04-26 14:03:59 +0300

    simple spa

--- javascript/04_simple_spa/public/script.js
+++ javascript/04_simple_spa/public/script.js
@@ -1,5 +1,6 @@
 window.onload = function() {
 	createForm();
+	getContactList();
 }
 
 createForm = () => {
@@ -74,7 +75,139 @@ createForm = () => {
 	form.appendChild(submitbutton);
 	form.addEventListener("submit",function(event) {
 		event.preventDefault();
-		console.log("Added a new contact");
+		addToList();
 	})
 	anchor.appendChild(form);
+}
+
+addToList = async () => {
+	let firstname = document.getElementById("firstnameinput");
+	let lastname = document.getElementById("lastnameinput");
+	let email = document.getElementById("emailinput");
+	let phone = document.getElementById("phoneinput");
+	let contact = {
+		"firstname":firstname.value,
+		"lastname":lastname.value,
+		"email":email.value,
+		"phone":phone.value
+	}
+	let method = "POST";
+	let url = "/api/contact";
+	let request = {
+		method:method,
+		mode:"cors",
+		headers:{"Content-type":"application/json"},
+		body:JSON.stringify(contact)
+	}
+	let response = await fetch(url,request);
+	if(response.ok) {
+		firstname.value = "";
+		lastname.value = "";
+		email.value = "";
+		phone.value = "";
+		getContactList();
+	} else {
+		console.log("Server responded with a status:",response.status);
+	}
+}
+
+getContactList = async () => {
+	let request = {
+		method:"GET",
+		mode:"cors",
+		headers:{"Content-type":"application/json"}
+	}
+	let response = await fetch("/api/contact",request);
+	if(response.ok) {
+		let data = await response.json();
+		populateTable(data);
+	} else {
+		console.log("Failed to load contacts. Server responded with a status:",response.status);
+	}
+}
+
+populateTable = (data) => {
+	let anchor = document.getElementById("anchor");
+	let oldTable = document.getElementById("table");
+	if(oldTable) {
+		anchor.removeChild(oldTable);
+	}
+	let table = document.createElement("table");
+	
+	//Table header
+	let header =  document.createElement("thead");
+	let headerRow = document.createElement("tr");
+	
+	let firstnameheader = document.createElement("th");
+	let firstnametext = document.createTextNode("First Name");
+	firstnameheader.appendChild(firstnametext);
+	
+	let lastnameheader = document.createElement("th");
+	let lastnametext = document.createTextNode("Last Name");
+	lastnameheader.appendChild(lastnametext);
+
+	let emailheader = document.createElement("th");
+	let emailtext = document.createTextNode("Email");
+	emailheader.appendChild(emailtext);
+
+	let phoneheader = document.createElement("th");
+	let phonetext = document.createTextNode("Phone");
+	phoneheader.appendChild(phonetext);
+	
+	let removeheader = document.createElement("th");
+	let removetext = document.createTextNode("Remove");
+	removeheader.appendChild(removetext);	
+
+	let editheader = document.createElement("th");
+	let edittext = document.createTextNode("Edit");
+	editheader.appendChild(edittext);
+
+	headerRow.appendChild(firstnameheader);
+	headerRow.appendChild(lastnameheader);
+	headerRow.appendChild(emailheader);
+	headerRow.appendChild(phoneheader);
+	headerRow.appendChild(removeheader);
+	headerRow.appendChild(editheader);
+	header.appendChild(headerRow);
+	table.appendChild(header);
+	
+	//table body
+	
+	let tablebody = document.createElement("tbody");
+	for(let i=0;i<data.length;i++) {
+		let tableRow = document.createElement("tr");
+		for (x in data[i]) {
+			if(x === "id") {
+				continue;
+			}
+			let column = document.createElement("td");
+			let info = document.createTextNode(data[i][x]);
+			column.appendChild(info);
+			tableRow.appendChild(column);
+		}
+		let removeColumn = document.createElement("td");
+		let removeButton = document.createElement("input");
+		removeButton.setAttribute("type","button");
+		removeButton.setAttribute("value","Remove");
+		removeButton.setAttribute("name",data[i].id);
+		removeButton.addEventListener("click",function(event) {
+			console.log("This will remove id:",event.target.name);
+		})
+		removeColumn.appendChild(removeButton);
+		
+		let editColumn = document.createElement("td");
+		let editButton = document.createElement("input");
+		editButton.setAttribute("type","button");
+		editButton.setAttribute("value","Edit");
+		editButton.addEventListener("click",function(event) {
+			console.log("This will edit object:",data[i])
+		})
+		editColumn.appendChild(editButton);
+		tableRow.appendChild(removeColumn);
+		tableRow.appendChild(editColumn);
+		tablebody.appendChild(tableRow)
+	}
+	table.appendChild(tablebody);
+	table.setAttribute("id","table");
+	anchor.appendChild(table);
 }
