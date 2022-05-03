e939f2fd8d584920a3bb7bf670d9b40d9bdaba2e
Andriy Tymchenko <silpol@gmail.com>
2022-05-03 10:26:23 +0300

    submitted project 03 react

---
+++ react/03_compound_component/src/components/Card.js
@@ -0,0 +1,40 @@
+import {useState} from 'react';
+import Square from './Square';
+import Label from './Label';
+
+const Card = (props) => {
+
+    const [state, setState] = useState({
+        color:"red"
+    })
+
+    let cardStyle = {
+        height:200,
+        width:150,
+        backgroundColor:"#FFF",
+        WebkitFilter:"drop-shadow(0px 5px 5px #666)",
+        filter:"drop-shadow(0px 5px 5px #666)"
+    }
+
+    const onColorChange = () => {
+        let color = "#";
+        const letters = "0123456789ABCDEF";
+        for(let i=0;i<6;i++) {
+            let temp=Math.floor(Math.random()*16);
+            color = color + letters[temp]
+        }
+        setState({
+            color:color
+        })
+    }
+
+    return(
+        <div style={cardStyle}>
+        <Square color={state.color}/>
+        <Label color={state.color}
+            onColorChange={onColorChange}/>
+        </div>
+    )
+}
+
+export default Card;
