0425c887a978f5584a5403d503a6aabe15e027be
Andriy Tymchenko <silpol@gmail.com>
2022-05-11 10:57:43 +0300

    added Login page in frontend - stage 2

--- react/50_shopping_app/02_stage_login/frontend/src/App.js
+++ react/50_shopping_app/02_stage_login/frontend/src/App.js
@@ -4,12 +4,17 @@ import {useState,useEffect} from 'react';
 import ShoppingForm from './components/ShoppingForm';
 import ShoppingList from './components/ShoppingList';
 import NavBar from './components/Navbar';
+import LoginPage from './components/LoginPage';
 import {Route, Routes} from 'react-router-dom';
 
 function App() {
 
     const [state, setState] = useState({
         list:[]
+        isLogged:false,
+        token:"",
+        loading:false,
+        error:""
     })
 
     const [urlRequest,setUrlRequest] = useState({
@@ -18,19 +23,44 @@ function App() {
         action:""
     })
 
+    const setLoading = (loading) => {
+        setState((state) => {
+            return {
+                ...state,
+                loading:loading,
+                error:error
+            }
+        })
+    }
+
+    const clearState = () => {
+        setState({
+            list:[],
+            isLogged:false,
+            token:"",
+            loading:false,
+            error:""
+        })
+    }
+
     useEffect(() => {
         const fetchData = async () => {
             if(!urlRequest.url) {
                 return;
              }
+             setLoading(true);
              let response = await fetch(urlRequest.url,urlRequest.request);
+             setLoading(false);
              if(response.ok) {
                 // Handle all different successful requests for the backend
                 switch(urlRequest.action) {
                     case "getlist":
                         let data = await response.json();
-                        setState({
-                            list:data
+                        setState((state) => {
+                            return {
+                                ...state,
+                                list:data
+                            }
                         })
                         return;
                     case "additem":
@@ -42,23 +72,51 @@ function App() {
                     case "edititem":
                         getShoppingList();
                         return;
+                    case "register":
+                        setError("Register success!");
+                        return;
+                    case "login":
+                        let token = await response.json();
+                        setState((state) => {
+                            return {
+                                ...state,
+                                isLogged:true,
+                                token:token.token
+                            }
+                        })
+                        getShoppingList(token.token);
+                        return;
                     default:
                         return;
                 }
              } else {
+                if(response.status === 403) {
+                    clearState();
+                    setError("Your session has expired. Logging you out!");
+                }
                 // Handle all different failed requests for the backend
                 switch(urlRequest.action) {
                     case "getlist":
-                        console.log("Failed to retrieve shopping list. Server responded with a status  ",response.status)
+                        setError("Failed to retrieve shopping list. Server responded with a status: "+response.status)
                         return;
                     case "additem":
-                        console.log("Failed to add new item. Server responded with a status  ",response.status)
+                        setError("Failed to add new item. Server responded with a status: "+response.status)
                         return;
                     case "removeitem":
-                        console.log("Failed to remove item. Server responded with a status  ",response.status)
+                        setError("Failed to remove item. Server responded with a status: "+response.status)
                         return;
                     case "edititem":
-                        console.log("Failed to edit item. Server responded with a status  ",response.status)
+                        setError("Failed to edit item. Server responded with a status: "+response.status)
+                        return;
+                    case "register":
+                        if(response.status === 409) {
+                            setError("Username already in use. Try another");
+                        } else {
+                            setError("Failed to register new user. Server responded with a status: "+response.status);
+                        }
+                        return;
+                    case "login":
+                        setError("Failed to login user. Server responded with a status: "+response.status);
                         return;
                     default:
                         return;
@@ -68,13 +126,46 @@ function App() {
         fetchData();
     },[urlRequest.url,urlRequest.request])
 
-    const getShoppingList = () => {
+    const register = (user) => {
+        setUrlRequest({
+            url:"/register",
+            request:{
+                method:"POST",
+                mode:"cors",
+                headers:{"Content-type":"application/json"},
+                body:JSON.stringify(user)
+            },
+            action:"register"
+        })
+
+    }
+
+    const login = (user) => {
+        setUrlRequest({
+            url:"/login",
+            request:{
+                method:"POST",
+                mode:"cors",
+                headers:{"Content-type":"application/json"},
+                body:JSON.stringify(user)
+            },
+            action:"login"
+       })
+
+    }
+
+    const getShoppingList = (token) => {
+        let temptoken = state.token;
+        if(token) {
+            temptoken = token
+        }
         setUrlRequest({
             url:"/api/shopping",
             request:{
                 method:"GET",
                 mode:"cors",
-                headers:{"Content-type":"application/json"}
+                headers:{"Content-type":"application/json",
+                        "token":temptoken}
             },
             action:"getlist"
         })
@@ -86,7 +177,8 @@ function App() {
             request:{
                 method:"POST",
                 mode:"cors",
-                headers:{"Content-type":"application/json"},
+                headers:{"Content-type":"application/json",
+                        "token":state.token},
                 body:JSON.stringify(item)
             },
             action:"additem"
@@ -99,7 +191,8 @@ function App() {
         request:{
             method:"DELETE",
             mode:"cors",
-            headers:{"Content-type":"application/json"}
+            headers:{"Content-type":"application/json",
+                    "token":state.token}
         },
         action:"removeitem"
     })
@@ -111,21 +204,39 @@ function App() {
             request:{
                 method:"PUT",
                 mode:"cors",
-                headers:{"Content-type":"application/json"},
+                headers:{"Content-type":"application/json",
+                        "token":state.token},
                 body:JSON.stringify(item)
             },
             action:"edititem"
         })
     }
 
+  // Conditional rendering
+
+  let messageArea = <h2></h2>
+  if (state.loading) {
+    messageArea = <h2>Loading...</h2>
+  }
+  let tempRender = <Routes>
+    <Route exact path="/" element={
+            <LoginPage setError={setError} register={register} login={login} />
+            }>
+    </Routes>
+  if(state.isLogged) {
+          tempRender = <Routes>
+              <Route exact path="/" element = { <ShoppingList list={state.list} removeFromList={removeFromList} editItem={editItem} /> } />
+              <Route path="/form" element = { <ShoppingForm addShoppingItem={addShoppingItem}/> } />
+          </Routes>
+  }
+
+
   return (
     <div className="App">
         <NavBar/>
+        {messageArea}
         <hr/>
-        <Routes>
-            <Route exact path="/" element = { <ShoppingList list={state.list} removeFromList={removeFromList} editItem={editItem} /> } />
-            <Route path="/form" element = { <ShoppingForm addShoppingItem={addShoppingItem}/> }/>
-        </Routes>
+        {tempRender}
     </div>
   );
 }
