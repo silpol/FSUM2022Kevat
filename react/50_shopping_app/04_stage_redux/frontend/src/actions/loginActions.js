e55be1a73585a7169d67f25a9a6fb5a925eb037b
Andriy Tymchenko <silpol@gmail.com>
2022-05-17 13:04:52 +0300

    initial frontend with Redux - stage 4

---
+++ react/50_shopping_app/04_stage_redux/frontend/src/actions/loginActions.js
@@ -0,0 +1,12 @@
+// Action types as constants
+
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAILED = "REGISTER_FAILED";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILED = "LOGIN_FAILED";
+export const LOGOUT_SUCCESS = "LOGOUT_SUCCESS";
+export const LOGOUT_FAILED = "LOGOUT_FAILED";
+export const LOADING = "LOADING";
+export const STOP_LOADING = "STOP_LOADING";
+export const CLEAR_LOGIN_STATE = "CLEAR_LOGIN_STATE";
+
