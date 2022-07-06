801da9b4eede36f3066aca048b3ca37d2d39723f
Andriy Tymchenko <silpol@gmail.com>
2022-07-06 11:58:39 +0300

    stage 05 - statemanager: fixes and additions

--- react/50_shopping_app/05_stage_statemanager/frontend/src/context/StateProvider.js
+++ react/50_shopping_app/05_stage_statemanager/frontend/src/context/StateProvider.js
@@ -1,7 +1,7 @@
 import { useReducer } from 'react';
 import AppStateContext from './AppStateContext';
 import ActionContext from './ActionContext';
-import * as ActionConstants from '..types/actionConstants';
+import * as ActionConstants from '../types/actionConstants';
 
 const getInitialState = () => {
     if(sessionStorage.getItem("state")) {
@@ -27,15 +27,115 @@ const saveToStorage = (state) => {
 }
 
 const listReducer = (state, action) => {
-    // TODO fill the reducer
+    let tempState = {};
+    switch (action.type) {
+        case ActionConstants.LOADING:
+            return {
+                ...state,
+                loading:true,
+                error:""
+            }
+        case ActionConstants.STOP_LOADING:
+            return {
+                ...state,
+                loading:false
+            }
+        case ActionConstants.REGISTER_SUCCESS:
+            tempState = {
+                ...state,
+                error:"Register success!"
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.REGISTER_FAILED:
+            tempState = {
+                ...state,
+                error:action.error
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.LOGIN_SUCCESS:
+            tempState = {
+                ...state,
+                token:action.token,
+                isLogged:true
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.LOGIN_FAILED:
+            tempState = {
+                ...state,
+                error:action.error
+            }
+            saveToStorage(tempState);
+            return tempState;    
+        case ActionConstants.FETCH_LIST_SUCCESS:
+            tempState = {
+                ...state,
+                list:action.list
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.FETCH_LIST_FAILED:
+            tempState = {
+                ...state,
+                error:action.error
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.ADD_ITEM_SUCCESS:
+            tempState = {
+                ...state,
+                error:""
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.ADD_ITEM_FAILED:
+            tempState = {
+                ...state,
+                error:action.error
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.REMOVE_ITEM_SUCCESS:
+            tempState = {
+                ...state,
+                error:""
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.REMOVE_ITEM_FAILED:
+            tempState = {
+                ...state,
+                error:action.error
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.EDIT_ITEM_SUCCESS:
+            tempState = {
+                ...state,
+                error:""
+            }
+            saveToStorage(tempState);
+            return tempState;
+        case ActionConstants.EDIT_ITEM_FAILED:
+            tempState = {
+                ...state,
+                error:action.error
+            }
+            saveToStorage(tempState);
+            return tempState;
+        default:
+            return state;            
+    }
 }
 
-const stateProvider = (props) => {
+const StateProvider = (props) => {
     const [state, dispatch] = useReducer(listReducer, initialState);
 
     return (
         <AppStateContext.Provider value={state}>
-           <ActionContext.Provider value={{dispatch:dispatch}}>
+            <ActionContext.Provider value={{dispatch:dispatch}}>
                 {props.children}
             </ActionContext.Provider> 
         </AppStateContext.Provider>
---
+++ react/50_shopping_app/05_stage_statemanager/frontend/src/hooks/useAction.js
@@ -0,0 +1,5 @@
+import { useContext, useEffect, useState } from "react";
+import ActionContext from "../context/ActionContext";
+import * as ActionConstants from "../types/actionConstants"
+import useAppState from "./useAppState";
+
