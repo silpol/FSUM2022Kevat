a367ad6d1655882c67b250f2963f2c3e2e2737bf
Andriy Tymchenko <silpol@gmail.com>
2022-07-11 10:02:34 +0300

    first steps

--- angular/todo/src/app/app.component.ts
+++ angular/todo/src/app/app.component.ts
@@ -2,9 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  /* templateUrl: './app.component.html', */
+  template: '<h1>Hello world! {{ myvariable }}</h1>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'todo';
+  myvariable = 'Pryvit';
 }
