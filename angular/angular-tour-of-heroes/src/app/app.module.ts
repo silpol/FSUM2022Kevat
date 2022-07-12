c72779ea636792f018bd322c8912be8bb9563c70
Andriy Tymchenko <silpol@gmail.com>
2022-07-12 10:38:02 +0300

    TOH - creating Hero interface

--- angular/angular-tour-of-heroes/src/app/app.module.ts
+++ angular/angular-tour-of-heroes/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { Hero } from './hero';
 
 @NgModule({
   declarations: [
