248388f95bfe249e2ef15b1890150524df265561
Andriy Tymchenko <silpol@gmail.com>
2022-07-12 14:06:31 +0300

    added basic service hero implemented

--- angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -14,16 +15,22 @@ export class HeroesComponent implements OnInit {
     name:'Windstorm'
   };
 
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
   
   onSelect(hero: Hero): void {
   this.selectedHero = hero;
   }
-  constructor() { }
+
+  getHeroes(): void {
+    this.heroes = this.heroService.getHeroes();
+  }
+
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
 
 }
