272edfa70197e63939da8ca2e6f7707c0594f5ab
Andriy Tymchenko <silpol@gmail.com>
2022-07-13 13:56:30 +0300

    TOH - add hero function

--- angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -13,10 +13,18 @@ export class HeroesComponent implements OnInit {
 
   heroes: Hero[] = [];
 
-
   getHeroes(): void {
     this.heroService.getHeroes()
         .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
   }
 
   constructor(private heroService: HeroService, private messageService: MessageService) { }
