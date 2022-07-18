bad8632eb83d5b3d4cff428a984ca305eea8f686
Andriy Tymchenko <silpol@gmail.com>
2022-07-18 13:48:17 +0300

    Shopping app with TSX implemented

--- react/60_typescript/03_forms_and_lists/src/App.tsx
+++ react/60_typescript/03_forms_and_lists/src/App.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ShoppingList from './components/ShoppingList';
+import ShoppingItem from './models/ShoppingItem';
+import ShoppingForm from './components/ShoppingForm';
+
+interface State {
+  list:ShoppingItem[];
+  id:number;
+}
 
 function App() {
+
+  const [state,setState] =useState<State>({
+    list:[],
+    id:100
+  })
+
+  const addItem = (item:ShoppingItem) => {
+    item.id=state.id;
+    setState((state) => {
+      return {
+        list:state.list.concat(item),
+        id:state.id+1
+      }
+    })
+  }
+
+  const removeItem = (id:number) => {
+    setState((state) => {
+      let tempList = state.list.filter(item => item.id !== id);
+      return {
+        ...state,
+        list:tempList
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ShoppingForm addItem={addItem}/>
+      <hr/>
+      <ShoppingList list={state.list} removeItem={removeItem}/>
     </div>
   );
 }
